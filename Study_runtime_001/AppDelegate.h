b7133d2e11badfa5f313b1e469d5f93eac067e69
YjjTT <yjjtt@yjjtt-2.local>
2018-04-10 09:54:49 +0800

    Initial Commit

---
+++ Study_runtime_001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Study_runtime_001
+//
+//  Created by YjjTT on 2018/4/10.
+//  Copyright © 2018年 YjjTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
